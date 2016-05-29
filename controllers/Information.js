c7c6580a44d234902d3656d91576ddff661e1453
Philipp Hug <philipp@hug.cx>
2016-05-29 23:23:27 +0200

    /info/userinfo api to get user about logged in user

--- controllers/Information.js
+++ controllers/Information.js
@@ -9,6 +9,34 @@ var cached = require('../static');
 // main index
 var db_regions = db('AMMNT_FIR');
 
+var User = require('../model/User');
+var Org = require('../model/Org');
+var Permission = require('../model/Permission');
+
+module.exports.getUserInfo = function (req, res, next) {
+  var id = 1; //req.swagger.params.id.value;
+
+  var user = new User();
+  user.load(undefined, id)
+  .then(function(result) {
+    var perm = new Permission();
+    return Promise.all([result, perm.load(undefined, id)]);
+  })
+  .then(function(data) {
+    var result = data[0];
+    result.permissions = data[1].permissions;
+    result.user_id = id;
+    delete(result.node_id);
+    delete(result.deleted);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result || {}, null, 2));
+  })
+  .catch(function(error) {
+    res.statusCode = 500;
+    return res.end(error.message);
+  });
+};
+
 module.exports.getRegions = function(req, res, next) {
   db_regions.select().then(function(result) {
     if (!result) {
